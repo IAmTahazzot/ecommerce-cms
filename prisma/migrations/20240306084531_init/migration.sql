c34c4f73c74d81ba2917c620c4251e5c4a966fa3
Tahazzot <iamtahazzot@gmail.com>
2024-03-06 15:38:03 +0600

    feat: established database connection using prisma ORM
    to see the ERD, you may visit the .erd file with erd extension in vscode

---
+++ prisma/migrations/20240306084531_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "bio" TEXT,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
