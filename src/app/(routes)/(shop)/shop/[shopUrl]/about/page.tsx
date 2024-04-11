e55f47c03b7753e2d682957ff6d074d38da4a5c9
Tahazzot <iamtahazzot@gmail.com>
2024-04-11 10:28:52 +0600

    feat: static about page for any shop

--- src/app/(routes)/(shop)/shop/[shopUrl]/about/page.tsx
+++ src/app/(routes)/(shop)/shop/[shopUrl]/about/page.tsx
@@ -1,12 +1,30 @@
+import Container from "@/components/Shop/Layout/Container";
+
 export default function AboutShopPage() {
   return (
-    <div>
-      <h1>About us</h1>
-      <p>
-        This is the about page for the shop. Here you can find information about
-        the shop and its owners. For now, it&apos;s not dynamic and just a
-        placeholder.
-      </p>
+    <div className="mt-7">
+      <Container>
+        <h1 className="text-7xl font-bold mb-4">About us</h1>
+        <p>
+          This is the about page for the shop. Here you can find information
+          about the shop and its owners. For now, it&apos;s not dynamic and just
+          a placeholder.
+        </p>
+        <p className="mt-5">
+          <span className="bg-green-500 text-white p-1 rounded"> ADs </span>
+          <span>
+            To build a large CMS like UnityShop. Contact us here:
+            <a
+              href="https://tahazzot.me/contact"
+              className="text-blue-500 hover:underline"
+              target="_blank"
+            >
+              {" "}
+              tahazzot.me/contact
+            </a>
+          </span>
+        </p>
+      </Container>
     </div>
   );
 }
